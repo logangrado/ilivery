1287856ad69fb857be7888bb31f35efbb99bd327
Logan Grado <logangrado@gmail.com>
2025-03-22 15:18:02 -0500

    Switch to UV

--- ilivery/__init__.py
+++ ilivery/__init__.py
@@ -1,8 +1,4 @@
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-__version__ = importlib_metadata.version(__name__)
+from ._version import __version__
 
 import matplotlib
 
