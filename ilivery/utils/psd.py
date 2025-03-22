cf810b4c2158d28280c0291651fdd885f07fb091
Logan Grado <logangrado@gmail.com>
2025-03-22 11:16:35 -0500

    Revert hard-coding of PSD size, fix tests with small layers

--- ilivery/utils/psd.py
+++ ilivery/utils/psd.py
@@ -210,7 +210,7 @@ def get_section_mask(expression, template):
     return section_mask, bbox
 
 
-def load_layers(path, groups=None, size=(2048, 2048)):
+def load_layers(path, groups=None):
     logger.info(f"Loading PSD layers: {path}")
 
     cache_base_path, cache_paths, checksum = _get_cache_path_and_checksum(path)
@@ -228,6 +228,7 @@ def load_layers(path, groups=None, size=(2048, 2048)):
             shutil.rmtree(cache_base_path)
         psd = PSDImage.open(path)
 
+        size = psd.size
         _cache_psd_recursive(cache_paths["images"], psd, size)
         # Write checksum
         with open(cache_paths["checksum"], "w") as f:
