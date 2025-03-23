6b7dcae6c9651f64affe52514ada9443760ea863
Logan Grado <logangrado@gmail.com>
2025-03-23 10:08:17 -0500

    Debug paths

--- ilivery/__init__.py
+++ ilivery/__init__.py
@@ -1,12 +1,17 @@
 from ._version import __version__
 
 import matplotlib
+import logging
+from pathlib import Path
+import loggerado
 
 matplotlib.use("agg")
 import matplotlib.pyplot
 
-import logging
-from pathlib import Path
+
+logger = logging.getLogger(__name__)
+loggerado.configure_logger(logger, level="INFO", ansi=True)
+
 
 ROOT = Path(__file__).parent
 RESOURCE_DIR = ROOT.parent / "resources"
@@ -14,9 +19,6 @@ TEMPLATE_DIR = RESOURCE_DIR / "templates"
 DECAL_DIR = RESOURCE_DIR / "decals"
 TEXTURE_DIR = RESOURCE_DIR / "textures"
 LAYER_CACHE_DIR = ROOT.parent / ".layer_cache"
-
-import loggerado
-
-logger = logging.getLogger(__name__)
-
-loggerado.configure_logger(logger, level="INFO", ansi=True)
+print(f"FILE: {__file__}")
+print(f"ROOT_DIR: {ROOT}")
+print(f"RESOURCE_DIR: {RESOURCE_DIR}")
