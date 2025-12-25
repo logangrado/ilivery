e26adeccd559c0669a05ded31ea4e02154e85f67
Logan Grado <grado.logan@gmail.com>
2025-12-25 14:48:26 -0600

    rename to src

--- src/ilivery/__init__.py
+++ src/ilivery/__init__.py
@@ -1,19 +1,19 @@
-from ._version import __version__
-
-import matplotlib
 import logging
 from pathlib import Path
+
 import loggerado
+import matplotlib
+
+from ._version import __version__
 
 matplotlib.use("agg")
 import matplotlib.pyplot
-
 
 logger = logging.getLogger(__name__)
 loggerado.configure_logger(logger, level="INFO", ansi=True)
 
 
-ROOT = Path(__file__).parent
+ROOT = Path(__file__).parent.parent
 RESOURCE_DIR = ROOT.parent / "resources"
 TEMPLATE_DIR = RESOURCE_DIR / "templates"
 DECAL_DIR = RESOURCE_DIR / "decals"
